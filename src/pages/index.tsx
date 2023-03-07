cc3c1a97f9252c745dd1bb3d60fa1c0dcea032fd
rodrfentanes21 <rfentanes@sga.pucminas.br>
2023-03-07 11:27:57 -0300

    minor change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,20 +17,20 @@ export default function Home() {
       <main className="w-full lg:w-screen h-screen flex flex-col">
         <h1 className="text-4xl text-center uppercase pt-5">Trabalho Eng Software 2 - Rodrigo e Henrique</h1>
         <div className="grow flex flex-col justify-center gap-5 items-center align-middle">
-          <Link href="/create" className="w-32 bg-sky-400 hover:bg-sky-700 rounded-full text-white text-xl text-center">
+          <Link href="/create" className="w-32 bg-sky-400 hover:bg-sky-700 rounded-full text-white text-xl text-center select-none">
             Criar
           </Link>
-          <Link href="/read" className="w-32 bg-sky-400 hover:bg-sky-700 rounded-full text-white text-xl text-center">
+          <Link href="/read" className="w-32 bg-sky-400 hover:bg-sky-700 rounded-full text-white text-xl text-center select-none">
             Ler
           </Link>
-          <Link href="/update" className="w-32 bg-sky-400 hover:bg-sky-700 rounded-full text-white text-xl text-center">
+          <Link href="/update" className="w-32 bg-sky-400 hover:bg-sky-700 rounded-full text-white text-xl text-center select-none">
             Atualizar
           </Link>
-          <Link href="/delete" className="w-32 bg-sky-400 hover:bg-sky-700 rounded-full text-white text-xl text-center">
+          <Link href="/delete" className="w-32 bg-sky-400 hover:bg-sky-700 rounded-full text-white text-xl text-center select-none">
             Deletar
           </Link>
         </div>
       </main>
     </>
   );
-}
+} 
